baabe4d520df513e85227d1c5480e2d01b5154cc
Lennin sabogal <lenninssp1021@gmail.com>
2024-08-10 11:37:05 -0400

    More menu stuff

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -12,16 +12,16 @@ interface MenuComponentProps {
 export const MenuComponent: FC<MenuComponentProps> = ({ menuItems }) => {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   return (
-    <div className="h-full flex p-3 flex-col justify-around items-start truncate">
+    <div className={cn("h-full flex p-3 flex-col justify-around items-start truncate",isMobile ? "p-1" : "p-3")}>
       {menuItems.map((item) => (
-        <div key={item.name} className="flex items-center truncate p-3">
+        <div key={item.name} className={cn("flex items-center truncate",isMobile ? "p-0 text-xs" : "p-3")}>
           <Link href={item.link} className="flex">
             <div className="hover:underline cursor-pointer flex items-center gap-2">
               <Icon
                 icon="tabler:hand-click"
-                className={cn("text-white", {
-                  "text-lg": isMobile,
-                  "text-xl": !isMobile,
+                className={cn("text-white text-xl", {
+                  "hidden": isMobile,
+                  "flex": !isMobile,
                 })}
               />
               {item.name}
