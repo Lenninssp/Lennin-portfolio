952273a83984d6b773f023ae124a572306c6a3eb
Lennin sabogal <lenninssp1021@gmail.com>
2024-08-10 11:04:55 -0400

    Small organization change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Pixelify_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 
-type Theme = 'light' | 'dark';
+type Theme = "light" | "dark";
 
 interface ThemeStyles {
   light: string;
@@ -17,11 +17,9 @@ interface UseThemeReturn {
 }
 
 const themeStyles: ThemeStyles = {
-  light: 'bg-gray-100 border border-gray-300',
-  dark: 'bg-gray-800 border border-gray-700'
+  light: "bg-gray-100 border border-gray-300",
+  dark: "bg-gray-800 border border-gray-700",
 };
-
-
 
 const pixelifySans = Pixelify_Sans({ subsets: ["latin"] });
 
@@ -52,21 +50,28 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const  style  =  themeStyles.dark;
+  const style = themeStyles.dark;
   return (
     <html lang="en">
       <body className={cn("bg-black text-white", pixelifySans.className)}>
         <Header />
         <BackgroundGrid />
-        <main> <div className="h-screen w-screen">
-      <div className="flex flex-col justify-center items-center w-full h-full">
-        <div
-          className={cn(
-            "bg-opacity-60 h-full md:h-5/6 md:w-10/12 lg:w-1/2 border animate-fade-up animate-delay-[2000ms] flex justify-center overflow-auto",
-            style
-          )}
-        >
-          <div className="h-fit w-full grid grid-cols-6 p-5 gap-5">{children}</div></div></div></div></main>
+        <main>
+          <div className="h-screen w-screen">
+            <div className="flex flex-col justify-center items-center w-full h-full">
+              <div
+                className={cn(
+                  "bg-opacity-60 h-full md:h-5/6 md:w-10/12 lg:w-1/2 border animate-fade-up animate-delay-[2000ms] flex justify-center overflow-auto",
+                  style
+                )}
+              >
+                <div className="h-fit w-full grid grid-cols-6 p-5 gap-5">
+                  {children}
+                </div>
+              </div>
+            </div>
+          </div>
+        </main>
       </body>
     </html>
   );
