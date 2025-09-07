fc5cd8a68fe6d7e3881a919a92d5c9f50c08cdb8
Lennin sabogal <lenninssp1021@gmail.com>
2025-09-07 16:55:23 -0400

    feat: adding fonts to project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 "use client";
-export const Home = () => {
+
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+const Home = () => {
+
+  const [text, setText] = useState(">");
 
   return (
-    <div>
-      Lennin s portfolio
+    <div className=" w-full flex flex-col">
+     {"> Lennin s portfolio"}
+     <Input onChange={(e) => setText(e.target.value)} content={text} placeholder=">" className="bg-transparent border-0 "/>
     </div>
   )
 }
+
+export default Home;
