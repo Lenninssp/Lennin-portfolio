ba90a7a86bfa7614dfbe95b0dfc15f8c0fdbc599
Lennin sabogal <lenninssp1021@gmail.com>
2025-09-08 20:14:19 -0400

    fix: small side bar adaptation with text line

--- src/components/general/text/text-line.tsx
+++ src/components/general/text/text-line.tsx
@@ -1,10 +1,13 @@
 import { cn } from "@/lib/utils";
 
-type TextType = "title" | "subTitle" | "text" | "link" | "importantText"
+type TextType = "title" | "subTitle" | "text" | "link" | "importantText";
+type TextColor = "white" | "red";
 
-export interface TextLineInterface extends React.HTMLAttributes<HTMLDivElement> {
+export interface TextLineInterface
+  extends React.HTMLAttributes<HTMLDivElement> {
   text: string;
-  type?: TextType ;
+  type?: TextType;
+  color?: TextColor
   className?: string;
   children?: React.ReactNode;
 }
@@ -12,23 +15,28 @@ export interface TextLineInterface extends React.HTMLAttributes<HTMLDivElement> 
 export const TextLine = ({
   text,
   type = "text",
+  color = "white",
   className,
   children,
   ...props
 }: TextLineInterface) => {
-
   const textFormat: Record<TextType, string> = {
     title: "text-2xl",
     subTitle: "text-xl text-red-500",
     text: "",
     importantText: "text-red-500",
-    link: "italic"
+    link: "italic",
+  };
+  
+  const textColor: Record<TextColor, string> = {
+    white: "text-white",
+    red: "text-red-500",
   }
- 
+
   return (
-    <div className={cn("", textFormat[type] ,className)} {...props}>
+    <div className={cn("", textFormat[type], textColor[color], className)} {...props}>
       {text}
       {children}
     </div>
-  )
-}
+  );
+};
