5ebed6bd24ae2ebb4ba7162051012697d9327098
Lennin sabogal <lenninssp1021@gmail.com>
2025-09-08 22:17:57 -0400

    feat: small title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 import type { Metadata } from "next";
 import { Pixelify_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { PageProvider } from "@/contexts/selected-page";
 import { Toaster } from "sonner";
+import { TextLine } from "@/components/general/text/text-line";
 
 export default function RootLayout({
   children,
@@ -18,9 +19,16 @@ export default function RootLayout({
           <PageProvider>
             <div className="h-screen w-screen">
               <div className="flex flex-col justify-center items-center w-full h-full">
+                <TextLine
+                  className=" absolute top-3 left-3"
+                  text="Lennin's Portfolio"
+                  type="title"
+                  color="red"
+                />
+
                 <div
                   className={cn(
-                    "bg-opacity-60 w-full h-full md:h-5/6 md:w-10/12 lg:w-2/3 border rounded-2xl  flex justify-center overflow-auto"
+                    "bg-opacity-60 relative w-full h-full md:h-5/6 md:w-10/12 lg:w-2/3 border rounded-2xl  flex justify-center overflow-auto"
                   )}
                 >
                   <div className="h-full w-full flex p-10">{children}</div>
@@ -29,7 +37,7 @@ export default function RootLayout({
             </div>
           </PageProvider>
         </main>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
   );
