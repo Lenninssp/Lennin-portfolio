94747f56d708ff4708398c2069403773843165f7
Lennin sabogal <lenninssp1021@gmail.com>
2025-09-10 16:58:51 -0400

    feat: changing to dono for the backend and prisma with postgres for the database

--- other-projects-backends/album-ranker/backend/src/index.ts
+++ other-projects-backends/album-ranker/backend/src/index.ts
@@ -1,19 +1,19 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
+import { Hono } from "hono";
+import { cors } from "hono/cors";
 
-const app = new Hono()
+const app = new Hono();
 
-app.use('/api/*', cors());
+app.use("/api/*", cors());
 
-app.get('/api/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/api/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-app.get('/api/hola', (c) => {
-  return c.text("Hi mom")
-})
+app.get("/api/hola", (c) => {
+  return c.text("Hi mom");
+});
 
 export default {
   port: 3001,
-  fetch: app.fetch
-}
+  fetch: app.fetch,
+};
