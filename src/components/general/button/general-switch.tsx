8579a151824f0d9e8a15f11b357ae346ef8ba96b
Lennin sabogal <lenninssp1021@gmail.com>
2025-09-10 18:25:50 -0400

    feat: adding switch to LenninUI

---
+++ src/components/general/button/general-switch.tsx
@@ -0,0 +1,34 @@
+import { Switch } from "@/components/ui/switch";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { TextColor } from "../color-format-router";
+import { cn } from "@/lib/utils";
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+
+
+interface GeneralSwitchProps {
+  activated: boolean;
+  onToggle: (value: boolean) => void;
+  icon1?: string;
+  icon2?: string;
+  className?: string
+}
+
+export const GeneralSwitch = ({
+  activated,
+  onToggle,
+  icon1,
+  icon2,
+  className
+}: GeneralSwitchProps) => {
+  return (
+    <div className={cn("flex w-fit gap-1 items-center", className)}>
+      {icon1 && <Icon icon={icon1} />}
+      <Switch
+        checked={activated}
+        onCheckedChange={onToggle}
+        className=""
+      />
+      {icon2 && <Icon icon={icon2} />}
+    </div>
+  );
+};
