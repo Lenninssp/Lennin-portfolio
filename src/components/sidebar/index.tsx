067bd11938447780164ed9a0efe6b19d86215343
Lennin sabogal <lenninssp1021@gmail.com>
2025-09-10 12:56:02 -0400

    refactor: little cleaning, i really gotta add prettier to the project

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -2,10 +2,9 @@ import { usePageContext } from "@/contexts/selected-page";
 import { SidebarFrame } from "./frame";
 import { SidebarButton } from "./sidebar-button";
 import { PagesEnum } from "@/enums/pages";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 import { RoundedLink } from "../general/rounded-link/rounded-link";
 import { handleEmailClick } from "../contact";
-import { Icon } from "@iconify/react/dist/iconify.js";
 import { GeneralIcon } from "../general/icon/general-icon";
 import { Button } from "../ui/button";
 import { cn } from "@/lib/utils";
@@ -18,23 +17,33 @@ export const Sidebar = () => {
     setHidden((prev) => !prev);
   };
 
-  const OpenSidebarButton = ({className}: {className?: string}) => (
-    <Button onClick={handleHidden} className={cn(" border-white rounded-xl border bg-black hover:bg-black", className)}>
-      <GeneralIcon icon={"material-symbols:view-sidebar-sharp"} color="red" className=" h-5 w-5" />
+  const OpenSidebarButton = ({ className }: { className?: string }) => (
+    <Button
+      onClick={handleHidden}
+      className={cn(
+        " border-transparent hover:border-white rounded-xl border bg-black hover:bg-black opacity-60 hover:opacity-100 p-1 px-3",
+        className
+      )}
+    >
+      <GeneralIcon
+        icon={"material-symbols:view-sidebar-sharp"}
+        color="red"
+        className=" h-5 w-5"
+      />
     </Button>
   );
 
   if (hidden)
     return (
       <div className="absolute top-2 left-2 md:relative h-full flex items-start">
-        <OpenSidebarButton className=" opacity-60 hover:opacity-100" />
+        <OpenSidebarButton/>
       </div>
     );
 
   return (
     <SidebarFrame className="absolute top-0 left-0 md:relative flex flex-col justify-between bg-black">
       <div className=" w-full flex flex-col items-start">
-        <OpenSidebarButton />
+        <OpenSidebarButton/>
         <SidebarButton
           text="Projects"
           selected={selectedPage === PagesEnum.PROJECTS}
